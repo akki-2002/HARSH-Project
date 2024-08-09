5ad2a5d8da1754f8f199c07dec41e37b80a56521
Akshat Agrawal <agrawalakshat582@gmail.com>
2024-08-09 16:15:53 +0530

    Login/Signup issue solved

--- frontend/src/components/Login Signup/Signup.js
+++ frontend/src/components/Login Signup/Signup.js
@@ -5,56 +5,12 @@ import emailImg from "../../components/Images/email.png";
 import lock from "../../components/Images/lock.png";
 import show from "../../components/Images/show.png";
 import hide from "../../components/Images/hide.png";
-import logo from "../../components/Images/logo.png"
+import logo from "../../components/Images/logo.png";
 
 function Signup() {
-  const [move, setMove] = useState();
-  const [move2, setMove2] = useState();
-
-  const [name, setName] = useState(""); // State for name
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
-  const handleNameClick = () => {
-    setMove({
-      top: "-15px",
-    });
-
-    if (!email) {
-      setMove2({
-        top: "13px",
-      });
-    }
-    setIsVisible(false);
-    setPassInput("password");
-  };
-
-  const handleEmailClick = () => {
-    setMove({
-      top: "-15px",
-    });
-
-    if (!password) {
-      setMove2({
-        top: "13px",
-      });
-    }
-    setIsVisible(false);
-    setPassInput("password");
-  };
-
-  const handlePasswordClick = () => {
-    setMove2({
-      top: "-15px",
-    });
-
-    if (!email) {
-      setMove({
-        top: "13px",
-      });
-    }
-  };
-
   const [isVisible, setIsVisible] = useState(false);
   const [passInput, setPassInput] = useState("password");
 
@@ -67,75 +23,78 @@ function Signup() {
     <>
       <div className="signupMain">
         <div className="signup">
-        <img src={logo} alt="" style={{ width: "20%" }}/>
+          <img src={logo} alt="logo" style={{ width: "20%" }} />
           <div className="signupHeading">Create Account</div>
-          <form action="">
-            {/* Name input field */}
-            <div className="signupContent" onClick={handleNameClick}>
-              <label htmlFor="" style={move}>
-                {" "}
-                <img src={emailImg} alt="email" />{" "}
+          <form>
+            <div className="signupContent">
+              <input
+                type="text"
+                placeholder=" "
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+              />
+              <label>
+                <img src={emailImg} alt="name" />
                 <p>
                   Name <span>*</span>
                 </p>
               </label>
-              <input
-                type="text"
-                onChange={(e) => setName(e.target.value)}
-                value={name}
-              />
             </div>
 
-            {/* Email input field */}
-            <div className="signupContent" onClick={handleEmailClick}>
-              <label htmlFor="" style={move}>
-                {" "}
-                <img src={emailImg} alt="email" />{" "}
+            <div className="signupContent">
+              <input
+                type="email"
+                placeholder=" "
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+              />
+              <label>
+                <img src={emailImg} alt="email" />
                 <p>
                   Email Address <span>*</span>
                 </p>
               </label>
-              <input
-                type="email"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-              />
             </div>
 
-            {/* Password input field */}
-            <div className="signupContent1" onClick={handlePasswordClick}>
-              <label htmlFor="" style={move2}>
-                {" "}
-                <img src={lock} alt="lock" />{" "}
-                <p>
-                  Password <span>*</span>
-                </p>
-              </label>
+            <div className="signupContent1">
               <div className="ip">
                 <input
                   type={passInput}
+                  placeholder=" "
                   onChange={(e) => setPassword(e.target.value)}
                   value={password}
                 />
-                <div className="eye">
-                  <img
-                    src={isVisible ? show : hide}
-                    alt=""
-                    onClick={toggleVisibility}
-                  />
+                <label>
+                  <img src={lock} alt="lock" />
+                  <p>
+                    Password <span>*</span>
+                  </p>
+                </label>
+                <div className="eye" onClick={toggleVisibility}>
+                  <img src={isVisible ? show : hide} alt="visibility toggle" />
                 </div>
               </div>
             </div>
 
             <div className="signupBtns">
               <div className="signupBtn">
-              <Link to={'/'} style={{ textDecoration: 'none',  cursor: 'pointer' }}><button>REGISTER</button></Link>
+                <Link
+                  to={"/"}
+                  style={{ textDecoration: "none", cursor: "pointer" }}
+                >
+                  <button>REGISTER</button>
+                </Link>
               </div>
 
-              <p>OR</p>
+              <p className="or">OR</p>
 
               <div className="loginBtn">
-                <Link to={'/signin'} style={{ textDecoration: 'none',  cursor: 'pointer' }}><button>LOG IN</button></Link>
+                <Link
+                  to={"/signin"}
+                  style={{ textDecoration: "none", cursor: "pointer" }}
+                >
+                  <button>LOG IN</button>
+                </Link>
               </div>
             </div>
           </form>
