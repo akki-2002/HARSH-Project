70d66d12f5504a430739dde470d4adbc37901eed
Akshat Agrawal <agrawalakshat582@gmail.com>
2024-08-16 22:17:52 +0530

    tp

--- backend/server.js
+++ backend/server.js
@@ -12,20 +12,18 @@ const crypto = require('crypto')
 
 const app = express();
 
-app.use(cors(
-  {
-    origin: ["https://harsh-project-api.vercel.app/"],
-    methods: ["POST", "GET"],
-    credentials: true
-  }
-));
+app.use(cors({
+  origin: ["https://harsh-project-api.vercel.app/"],
+  methods: ["POST", "GET"],
+  credentials: true
+}));
 
 const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+// app.use(cors());
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/uploads', express.static('uploads'));
@@ -81,9 +79,10 @@ mongoose.connect(process.env.MONGODB_URI)
   .catch(err => console.log(err));
 
 // Basic route
-// app.get('/', (req, res) => {
-//   res.send('Hello from the backend!');
-// });
+app.get('/', (req, res) => {
+  res.send('Hello from the backend!');
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
