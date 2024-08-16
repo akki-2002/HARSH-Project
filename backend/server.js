fd918c80830bb8e640bd791178e19ee22018772d
Akshat Agrawal <agrawalakshat582@gmail.com>
2024-08-16 22:05:34 +0530

    tp

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(cors(
   {
-    origin: ["https://deploy-mern-1whq.vercel.app"],
+    origin: ["https://harsh-project-api.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true
   }
